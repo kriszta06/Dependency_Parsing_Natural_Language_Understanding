8d1ab1c6dd63c10e154c3c1adcb3e1ff62716344
NumeleTau <emailul_tau@exemplu.com>
2025-12-08 18:10:41 +0100

    push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from conllu_reader import ConlluReader
-from algorithm import ArcEager
+from algorithm import ArcEager, build_samples
 
 def read_file(reader, path, inference):
     trees = reader.read_conllu_file(path, inference)
@@ -12,15 +12,14 @@ def read_file(reader, path, inference):
     return trees
 
 
-"""
-ALREADY IMPLEMENTED
+"""f 
 Read and convert CoNLLU files into tree structures
 """
 # Initialize the ConlluReader
 reader = ConlluReader()
 train_trees = read_file(reader,path="en_partut-ud-train_clean.conllu", inference=False)
 dev_trees = read_file(reader,path="en_partut-ud-dev_clean.conllu", inference=False)
-test_trees = read_file(reader,path="en_partut-ud-test_clean.conllu", inference=True)
+test_trees = read_file(reader,pxth="en_partut-ud-test_clean.conllu", inference=True)
 
 """
 We remove the non-projective sentences from the training and development set,
@@ -60,3 +59,48 @@ print ("\n ------ TODO: Implement the rest of the assignment ------")
 # 2. Specify the file path: path = "<YOUR_PATH_TO_OUTPUT_FILE>"
 # 3. Process the file: trees = postprocessor.postprocess(path)
 # 4. Save the processed trees to a new output file.
+
+
+"""
+Below we will have described the architecture for the steps needed to be impemented
+in order to extract neede dataset(gold transitions) from parsing of each sentence
+using oracle function(arc eager alghoritm).
+For each sentence:
+1.we start with the initial state
+2.we iterate thorugh steps while we got to final state
+3.at each iteration of parsing we get the gold action with oracle function from arceager class
+4.transofrm it to features for our model
+5.append it in the dataset variable
+(same for train and dev)
+
+"""
+"""
+####################
+# FOR TRAIN DATASET#
+####################
+train_dataset = []
+
+        for sent in train_trees:
+            gold_samples = arc_eager.oracle(sent)
+
+            for sample in gold_samples:
+                feats = sample.state_to_feats()
+                gold_action = sample.transition.action
+                train_dataset.append((feats, gold_action))
+
+####################
+# FOR DEV DATASET#
+####################
+dev_dataset = []
+
+        for sent in dev_trees:
+            gold_samples = arc_eager.oracle(sent)
+            for sample in gold_samples:
+                feats = sample.state_to_feats()
+                gold_action = sample.transition.action
+                dev_dataset.append((feats, gold_action))
+"""
+
+"""
+
+"""
